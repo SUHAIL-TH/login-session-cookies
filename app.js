98f6dd196a5210c5c7471c70f1742fd33cc05f92
suhail th <suhailth17756@gmail.com>
2023-02-22 08:17:09 +0530

    3rd commit

--- app.js
+++ app.js
@@ -8,8 +8,6 @@ const app=express()
 app.set(cookieParser());
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
-
-
 app.use(session({
     secret: "thisismysecrctekeyfhrgfgrfrty84fwir767",
     saveUninitialized:true,
@@ -28,6 +26,7 @@ app.use((req, res, next) => {
 
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
 let sessions;
 let useremail="ami@gmail.com"
 let password="123456"
@@ -46,7 +45,6 @@ app.post("/login",(req,res)=>{
     if(req.body.email===useremail && req.body.password===password){
         sessions=req.session
         sessions.username=req.body.email
-        console.log(req.session);
         res.render("home")
     }else{
         res.render("login",{error:"invalid username or password"})
